855d5dcd3daae9c30e8ca38df212f99726b53cf9
ShawnJin96 <53147069+ShawnJin96@users.noreply.github.com>
2019-08-08 20:15:47 +1000

    Project1

---
+++ MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System;
+
+namespace MoviesGuideApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ImageView addButton;
+        ImageView searchButton;
+        EditText searchText;
+        RecyclerView myRecyclerView;
+      
+       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+          
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            myRecyclerView = (RecyclerView)FindViewById(Resource.Id.myRecyclerView);
+            searchButton = (ImageView)FindViewById(Resource.Id.searchButton);
+            searchText = (ImageView)FindViewById(Resource.Id.addNewButton);
+            searchText = (EditText)FindViewById(Resource.Id.searchText);
+
+            searchButton.Click += SearchButton_Click;
+        }
+
+       
+      
+
+        private void  SearchButton_Click(object sender, System.EventArgs e)
+        {
+            if (searchText.Visibility == Android.Views.ViewStates.Gone)
+            {
+                searchText.Visibility = Android.Views.ViewStates.Visible;
+            }
+            else
+            {
+                searchText.ClearFocus();
+                searchText.Visibility = Android.ViewStates.Gone;
+
+            }
+        }
+
+    }
+}
